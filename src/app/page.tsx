9fe19556bfa40fb90e09d88492d40ec5a0bfa5c2
fukada-m <fukada.m8888@gmail.com>
2024-03-22 18:57:20 +0900

    一旦まっさら

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,21 @@
-'use client'
+'use server'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {useState} from 'react'
 import Link from 'next/link'
 
-
-export default function Home() {
-  let [input, setInput] = useState("")
-  let [message, setMessage] = useState("your name")
-
-  const doChange = (event)=> {
-    setInput(event.target.value)
-  } 
-
-  const doClick = ()=> {
-    setMessage("Hello," + input + "!!")
-    setInput("")
-  }
-
+export default async function Home() {
   return (
     <main className="container" >
       <h1 className="title bg-primary">Next.jsのサンプル</h1>
       <p className="text-lg">Topぺーじ</p>
       <div>
-        <Link href="/pati">ぱっちのぺーじ</Link>
+        <form className='form' action={serverAction}>
+        <input className='input border' type="number" name='input' />
+        <button className='btn'>Click</button>
+        </form>
       </div>
-      <p>{message}</p>
       <div>
-        <input type="text" onChange={doChange} value={input} className="border-solid border-2 border-gray-400"/>
-        <button onClick={doClick} className="px-7 bg-blue-800 mx-2 text-white rounded-lg py-2">決定</button>
+        <Link href="/pati">ぱっちのぺーじ</Link>
       </div>
     </main>
   );
