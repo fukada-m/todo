798b61944b1d933d8fe9062a89ae06528b356aa9
fukada-m <fukada.m8888@gmail.com>
2024-03-22 13:27:45 +0900

    静的なページを作ってみた

--- src/app/name/[name]/page.tsx
+++ src/app/name/[name]/page.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Name({params}: {params:{name: string}}) {
   return (
     <main>
-      <h1>nameのダイナミックテスト</h1>
+      <h1 className='bg-primary'>nameのダイナミックテスト</h1>
       <p>あなたの名前は{params.name}だよ</p>
       <div>
         <Link href="/">トップぺーじ</Link>
