8a44b403435b645ede91f8743d4b246d5b986846
fukada-m <fukada.m8888@gmail.com>
2024-03-24 00:01:17 +0900

    メモを4つに固定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,32 +2,44 @@
 
 import { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import  { getMemo1, saveMemo1 } from './serverAction'
-import { get } from 'http';
+import  { getMemo, saveMemo } from './serverAction'
 
 export default function Home() {
-  let [memo1, setMemo1] = useState('');
-  let [memo2, setMemo2] = useState('');
-  let [memo3, setMemo3] = useState('');
-  let [memo4, setMemo4] = useState('');
+  const [memo1, setMemo1] = useState('');
+  const [memo2, setMemo2] = useState('');
+  const [memo3, setMemo3] = useState('');
+  const [memo4, setMemo4] = useState('');
+  const memos = [memo1, memo2, memo3, memo4];
   
-  const editMemo1 = (e:any) => {
-    setMemo1(e.target.value);
+  const editMemo = (index: number, e:any) => {
+    const value = e.target.value;
+    if (index === 0) setMemo1(value);
+    else if (index === 1) setMemo2(value);
+    else if (index === 2) setMemo3(value);
+    else if (index === 3) setMemo4(value);
   }
 
   useEffect(() => {
-    getMemo1().then((data) => {
-      setMemo1(data)
+      getMemo().then((data) => {
+        setMemo1(String(data[0]));
+        setMemo2(String(data[1]));
+        setMemo3(String(data[2]));
+        setMemo4(String(data[3]));
     })
   }, [])
 
+  useEffect(() => {
+    if (memo1 === '' && memo2 === '' && memo3 === '' && memo4 === '') {
+      return;
+    }
+    saveMemo(memos)
+  }, [memos])
+
   return (
     <main className="container" >
-        <form className='memo1' action={saveMemo1}>
-          <textarea className='border-solid border-2 d-block' onChange={editMemo1} value={memo1} rows={25} cols={40}  ></textarea>
-          <button >保存</button>
-        </form>
-
+          {memos.map((memo, index) => (
+            <textarea key={index} className='border-solid border-2 m-1' onChange={(e) => editMemo(index, e)} name={memo} value={memo} rows={25} cols={33}></textarea>
+          ))}
     </main>
   );
 }
--- src/app/serverAction.tsx
+++ src/app/serverAction.tsx
@@ -1,22 +1,24 @@
 "use server"
+
 import fs from 'fs';
 
-const fname = './public/memo1.txt';
+const fname1 = './public/memo1.txt';
+const fname2 = './public/memo2.txt';
+const fname3 = './public/memo3.txt';
+const fname4 = './public/memo4.txt';
 
-export async function saveMemo1(form:any) {
-  try {
-    const input = form.get('memo1');
-    fs.appendFileSync(fname, input + '\n');
-    return '保存しました';
-  }catch (e:any) {
-    return e.message;
-  }
+export async function saveMemo(memos: string[]): Promise<void>{
+      fs.writeFileSync(fname1, memos[0], 'utf-8');
+      fs.writeFileSync(fname2, memos[1], 'utf-8');
+      fs.writeFileSync(fname3, memos[2], 'utf-8');
+      fs.writeFileSync(fname4, memos[3], 'utf-8');
 }
 
-export async function getMemo1() {
-  try {
-    return fs.readFileSync(fname, 'utf-8');
-  } catch (e:any) {
-    return e.message;
-  }
+export async function getMemo(): Promise<string[]>{
+  const memos = [];
+  memos.push(fs.readFileSync(fname1, 'utf-8'));
+  memos.push(fs.readFileSync(fname2, 'utf-8'));
+  memos.push(fs.readFileSync(fname3, 'utf-8'));
+  memos.push(fs.readFileSync(fname4, 'utf-8'));
+  return memos;
 }
